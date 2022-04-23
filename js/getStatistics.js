ceb3f748262dfdccd680101f40839fd9286ac062
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-04-23 20:29:35 +0200

    fix

--- js/getStatistics.js
+++ js/getStatistics.js
@@ -26,6 +26,15 @@ const getExpenses = async () => {
 const refreshBtn = document.getElementById("refresh-btn");
 refreshBtn.addEventListener("click", getExpenses, false)
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+  
+async function loadStats() {
+    await delay(1500);
+    getExpenses();
+}
+
 window.onload = function() {
-    refreshBtn.click();
+    loadStats();
 };
